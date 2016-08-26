a654e5eee07127aefcef07ac659248fcbf5dc4c9
Kirill Osenkov <github@osenkov.com>
2016-08-25 17:46:29 -0700

    Add a console app project for easier testing.

---
+++ src/Microsoft.Language.Xml.ConsoleTestHost/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace Microsoft.Language.Xml.ConsoleTestHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = File.ReadAllText(@"D:\1.xml");
+            var root = Parser.ParseText(text);
+            Console.ReadKey();
+        }
+    }
+}
